2db4b9f3b34792a328e1a6cb139978e2bf032086
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2026-07-30 00:03:59 +0530

    REST API

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS employee (
+                                        id INT AUTO_INCREMENT PRIMARY KEY,
+                                        name VARCHAR(50) NOT NULL,
+    department VARCHAR(100) NOT NULL,
+    salary DOUBLE NOT NULL
+    );
